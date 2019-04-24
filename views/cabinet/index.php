8e815bf64af7cfe75b920874cc04c759096fd494
AlexeiTarasov323 <49696954+AlexeiTarasov323@users.noreply.github.com>
2019-04-24 19:03:18 +0300

    В меню добавлена панель управления, добавлен footer

--- views/cabinet/index.php
+++ views/cabinet/index.php
@@ -3,12 +3,18 @@
 <section>
     <div id="categoria">
         <div id="menu3">
-            <ul>              
+            <ul>
+
                 <h3>Пользователь <?php echo $user['name'];?>!</h3>
+                <?php if ($user['role'] == 'admin'): ?>
+					<li><a href="/admin/">Панель управления</a></li>
+				<?php endif; ?>     
                 <li><a href="/cabinet/locate/">Подать объявление</a></li>
-                <li><a href="/cabinet/edit/">Редактировать данные</a></li>              
+                <li><a href="/cabinet/edit/">Редактировать данные</a></li>         
             </ul>
         </div>
     </div>
 </section>
+
+<?php include ROOT . '/views/layouts/footer.php'; ?>
 
