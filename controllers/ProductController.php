c96b58e62e768ad1067f9ca64365d154ac632b1f
AlexeiTarasov323 <49696954+AlexeiTarasov323@users.noreply.github.com>
2019-04-24 16:03:17 +0300

    Update ProductController.php
    Добавлена проверка  переменной на пустоту

--- controllers/ProductController.php
+++ controllers/ProductController.php
@@ -9,8 +9,11 @@ class ProductController
 
         $categories = array();
         $categories = Category::getCategoriesList();
-        
+
         $product = Product::getProductById($productId);
+    	if (!$product) {
+    		header("Location: /product/");
+    	}
 
         require_once(ROOT . '/views/product/view.php');
 
