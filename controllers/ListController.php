a560c2003b2d985218759c1cb4250fc6f146d6ec
AlexeiTarasov323 <49696954+AlexeiTarasov323@users.noreply.github.com>
2019-04-24 19:51:22 +0300

    Внесены изменения в метод actionCategory()

--- controllers/ListController.php
+++ controllers/ListController.php
@@ -1,7 +1,7 @@
 <?php
 
 class ListController
-{
+{   
 
     // Выводит каждую категорию, параметром принимает ID категории
     public function actionCategory($categoryId, $page = 1)
@@ -10,16 +10,20 @@ class ListController
         //Получаем категории для меню 
         $categories = array();
         $categories = Category::getCategoriesList();
+
         
         //Выводит список объявлений
         $categoryProducts = array();
         $categoryProducts = Product::getProductsListByCategory($categoryId, $page);
 
+        if(!$categoryProducts) {          
+            header("Location: /error404/");       
+        }        
 
         //Количество товаров в текущей категории
         $total = Product::getTotalProductsInCategory($categoryId);
 
-        // Создаем объект Pagination - постраничная навигация
+        //Создаем объект Pagination - постраничная навигация
         $pagination = new Pagination($total, $page, Product::SHOW_BY_DEFAULT, 'page-');
 
          require_once(ROOT . '/views/catalog/category.php');
